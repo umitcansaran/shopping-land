0dc9bd1bf8870bc898aa5933b4df5dc15f6d7f2f
umitcansaran <umitcansaran@yahoo.com>
2025-01-25 16:43:38 +0100

    Change header title

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -49,7 +49,7 @@ const Header = () => {
             padding: "0.2rem 0.7rem",
           }}
         >
-          <strong>shopping land test</strong>
+          <strong>shopping land testing phase 2</strong>
         </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="basic-navbar-nav"
